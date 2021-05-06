ea39e423e0d2c2e7cc5efa338a4f4b5f0fb73e8b
MiloshN <miloskm@live.com>
2021-05-06 09:12:31 +0200

    auth controller

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,19 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::get('/', function () {
+        return "Hello World";
+    });
 });
+
+
+
+Route::get('/', function () {
+    return 'Hello World';
+});
+
+
