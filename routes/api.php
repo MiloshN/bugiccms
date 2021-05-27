23c7970e26f004d211635a9a430254c30c5d9d8b
MiloshN <miloskm@live.com>
2021-05-27 13:13:10 +0200

    little fix of route api.php

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,12 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:api')->post('logout', [AuthController::class, 'logout']);
     Route::middleware('auth:api')->post('refresh', [AuthController::class, 'refresh']);
     Route::post('register', [AuthController::class, 'register']);
-
-    Route::middleware('role:admin')->get('admin',function() {
-        return 'Welcome Admin';
+    Route::middleware('role:admin')->group(function () {
+        Route::get('admin', function() {
+            return 'Welcome Admin';
+        });
     });
-    Route::middleware('role:user')->get('',function() {
+    Route::middleware('role:user')->get('dashboard',function() {
         return 'Welcome User';
     });
 
